ae2c99d144313693e0545da883b413cb4c97a885
Ricardo <ric.dalvarez@hotmail.com>
2023-09-05 01:18:02 +0200

    adding react

---
+++ client/src/App.tsx
@@ -0,0 +1,38 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="app flex">
+      <img src="../public/background.png" alt="" className="fullscreen-img" />
+      <div className="help-section flex flex-row">
+        <div className="post-music">
+          <div className="music">
+            <h1>kiss me</h1>
+          </div>
+          <div className="post">
+            <h1>Post on the internet</h1>
+          </div>
+        </div>
+        <div className="login">
+          <h1>Login or talk with us directly</h1>
+          <button onClick={() => console.log("hello world")} >New user? register</button>
+          <label htmlFor="email">
+            <input type="text" placeholder='email' />
+          </label>
+          <label htmlFor="password">
+            <input type="passworld" placeholder='passworld' />
+          </label>
+          <button>Login</button>
+          <button>Talk</button>
+        </div>
+        <div className="painting-phrase">
+          <img src="" alt="an image here" />
+          <p>Everybody is different</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
