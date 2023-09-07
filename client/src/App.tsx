d7aaab594cf3841f7a718347daa1e7d98ecc253e
Ricardo <ric.dalvarez@hotmail.com>
2023-09-08 00:04:32 +0200

    adding random heroe name

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,45 @@
 import './App.css'
+import { useState, useEffect } from "react";
+
 
 function App() {
+  const [name, setName] = useState();
+  const url = 'https://superhero-search.p.rapidapi.com/api/heroes';
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': import.meta.env.VITE_REACT_APP_RAPIDAPI_KEY,
+      'X-RapidAPI-Host': 'superhero-search.p.rapidapi.com'
+    }
+  };
+
+  useEffect(() => {
+    const fetchDataFromAPI = async () => {
+      try {
+        const response = await fetch(url, options);
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data)
+          setName(data[0].name);
+
+        } else {
+          console.error('Failed to fetch data:', response.status);
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchDataFromAPI();
+
+  }, [])
+
+
 
   return (
     <div className="app flex flex-col">
       <img src="/background.png" alt="" className="fullscreen-img" />
-      <h1 className="absolute top-0 left-0 w-full h-full text-center text-white text-4xl flex items-center justify-center"> Hello Spider Man</h1>
+      <h1 className="absolute top-0 left-0 w-full h-full text-center text-white text-4xl flex items-center justify-center"> Hello {name}</h1>
       <div className="help-section flex flex-row">
         <div className="post-music bg-red-300">
           <div className="music">
