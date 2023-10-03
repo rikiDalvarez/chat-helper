a3b885f751e51b0a6c8a236596c2e32976d63e2b
rikiDalvarez <ric.dalvarez@hotmail.com>
2023-10-03 10:35:18 +0200

    add .env info and variables .env for google oauth2.0

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 
 # Depression and Suicidal Thoughts Support Chat App
+
+.env file:
+```env
+MONGO_URI="yourMongodburi"
+MONGO_URI_TEST="mongodbUriForTest"
+NODE_ENV="development"
+DATABASE_ENV='mongo'
+TEST_DATABASE='test'
+DATABASE="chat-helper"
+PORT="5000"
+JWT_SECRET='yoursecret'
+GOOGLE_CLIENT_ID="yourGoogleClientId"
+GOOGLE_SECRET_ID="yourGoogleSecretId"
+GOOGLE_OAUTH_REDIRECT_URL="yourGoogleRedirectURL"
+```
--- server/config/config.ts
+++ server/config/config.ts
@@ -15,6 +15,9 @@ interface ENV {
   DATABASE: string | undefined;
   TEST_DATABASE: string | undefined;
   JWT_SECRET: string | undefined;
+  GOOGLE_CLIENT_ID: string | undefined;
+  GOOGLE_SECRET_ID: string | undefined;
+  GOOGLE_OAUTH_REDIRECT_URL: string | undefined;
 }
 
 interface Config {
@@ -25,6 +28,9 @@ interface Config {
   DATABASE: string;
   TEST_DATABASE: string;
   JWT_SECRET: string;
+  GOOGLE_CLIENT_ID: string | undefined;
+  GOOGLE_SECRET_ID: string | undefined;
+  GOOGLE_OAUTH_REDIRECT_URL: string | undefined;
 }
 
 // Loading process.env as ENV interface
@@ -39,10 +45,13 @@ const getConfig = (): ENV => {
     PORT: process.env.PORT ? Number(process.env.PORT) : undefined,
     MONGO_URI: process.env.MONGO_URI,
     JWT_SECRET: process.env.JWT_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_SECRET_ID: process.env.GOOGLE_SECRET_ID,
+    GOOGLE_OAUTH_REDIRECT_URL: process.env.GOOGLE_OAUTH_REDIRECT_URL
   };
 };
-
-const getSanitizedConfig = (config: ENV): Config => {
+    
+    const getSanitizedConfig = (config: ENV): Config => {
   for (const [key, value] of Object.entries(config)) {
     if (value === undefined) {
       throw new Error(`Missing key ${key} in config.env`);
